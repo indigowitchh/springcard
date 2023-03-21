0deb6aa3a0e3b68dea50c2e69c6b08a1b1aead14
rey <114239691+indigowitchh@users.noreply.github.com>
2023-03-21 09:36:23 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main() {
 	//randomizer
 	srand(time(NULL));
 	int flight = rand() % 100 +1;
-
+	int colour = rand() % 100 + 1;
 
 	//Arrays
 	int birbColor[] = { 255, 192, 203 }; //pink color array
@@ -15,8 +15,13 @@ int main() {
 
 	int y[] = { 100,200,300,400,500,600 }; //ypos array
 
+	int FlowerColor[] = { 203,195,227 };
+	int FlowerColor2[] = { 162,255,197 };
+	int FlowerColor3[] = { 68,150,221 };
 
-	//instantiate birbs
+	
+	
+	//instantiate objects
 	birb Jakie(100, y[0], birbColor);
 	birb Alex(200, y[1], birbColor2);
 	birb Keven(300, y[2], birbColor3);
@@ -24,11 +29,18 @@ int main() {
 	birb Lucks(500, y[4], birbColor2);
 	birb Rickie(600, y[5], birbColor3);
 
-	sf::RenderWindow window(sf::VideoMode(700, 700), "Happy Spring!"); //set up game window
+	Flower Bobby(100, 700, FlowerColor);
+	Flower Milkies(250, 700, FlowerColor2);
+	Flower Wandit(400, 700, FlowerColor3);
+	Flower NoBallz(550, 700, FlowerColor);
+	Flower Tippers(700, 700, FlowerColor2);
+
+	
+	sf::RenderWindow window(sf::VideoMode(800, 800), "Happy Spring!"); //set up game window
 
 
 	while (true) {//GAME LOOP OMG
-		window.clear();
+		window.clear(sf::Color(218, 233, 246));
 
 		//fly
 		Jakie.fly(window);
@@ -45,8 +57,25 @@ int main() {
 		Jebis.draw(window);
 		Lucks.draw(window);
 		Rickie.draw(window);
+		
+
+		Bobby.draw(window);
+		Milkies.draw(window);
+		Wandit.draw(window);
+		NoBallz.draw(window);
+		Tippers.draw(window);
+		
+
+		//Change color
+		Bobby.changeColor(window);
+		Milkies.changeColor(window);
+		Wandit.changeColor(window);
+		NoBallz.changeColor(window);
+		Tippers.changeColor(window);
+
 
 		window.display();
 	}
 }
+
 
