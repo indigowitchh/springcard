bb363aed17f5e1edbf3c90840b95065aa6bdf624
rey <114239691+indigowitchh@users.noreply.github.com>
2023-03-21 09:36:04 -0600

    Update birb.h

--- birb.h
+++ birb.h
@@ -23,3 +23,24 @@ public: //public stuff can be seen/used by errbody in the clurb
 	void draw(sf::RenderWindow& window);
 	void fly(sf::RenderWindow& window);
 };
+
+
+class Flower {
+
+private:
+	int xpos;
+	int ypos;
+	int color[3];
+
+	sf::CircleShape mid;
+	sf::CircleShape petal1;
+	sf::CircleShape petal2;
+	sf::CircleShape petal3;
+	sf::CircleShape petal4;
+	sf::CircleShape petal5;
+
+public:
+	Flower(int x, int y, int c[3]);
+	void draw(sf::RenderWindow& window);
+	void changeColor(sf::RenderWindow& window);
+};
